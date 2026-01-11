966e8300b3abeb06547958c40e4d9d851897dae7
Danya <danilkotop800@gmail.com>
2026-01-11 23:21:10 +0200

    feat: change dev port to 4000 and add API proxy to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api-proxy/:path*',
+        destination: 'https://stork-helpers-api.onrender.com/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
