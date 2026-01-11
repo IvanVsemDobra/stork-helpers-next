558b7f0228c238adc82ae456b2283bb4c2f74d47
Anton Zorkin <don_er@ukr.net>
2026-01-11 23:15:46 +0200

    Mom tip card (#32)
    * Add MomTipCard component
    
    * feat: add weeks service and API types
    
    * MomTipCard
    
    ---------
    
    Co-authored-by: Anton Zorkin <zanton1978@gmail.com>

---
+++ components/MomTipCard/mom-tip-card.tsx
@@ -0,0 +1,17 @@
+import css from './mom-tip-card.module.css'
+
+interface momDailyTipsProps {
+  tipIndex: number
+  momDailyTips: string[]
+}
+export const MomTipCard = ({ tipIndex, momDailyTips }: momDailyTipsProps) => {
+  return (
+    <div className={css.card}>
+      <h2 className={css.title}>Порада для мами</h2>
+      <p className={css.text}>
+        {momDailyTips[tipIndex] ? momDailyTips[tipIndex] : 'Порада відсутня'}
+      </p>
+    </div>
+  )
+}
+
