095fcb556eee686a7a4b90d50323db1c1112e8c6
Nataliia Tkachenko <tkachenko_nataliya83@ukr.net>
2026-01-11 14:55:02 +0200

    feature/DashboardPage-grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
+import css from './page.module.css'
+// Містить в собі компоненти:
+// GreetingBlock,
+// StatusBlock,
+// BabyTodayCard,
+// MomTipCard,
+// TasksReminderCard,
+// FeelingCheckCard,
+
+// Загальна поведінка блоків на сторінці:
+// Десктоп:
+// Всі блоки-компоненти на цій сторінці повинні мати статичну (фіксовану) висоту згідно з макетом. У разі, якщо внутрішній контент перевищує висоту блоку, всередині блоку повинен з'являтись вертикальний скрол.
+// Планшет та мобілка:
+// Висота блоків динамічно змінюється відповідно до кількості контенту.
+
 export default function DashboardPage() {
   return (
-    <div>
-      DashboardPage
-      <div>
-        На цю сторінку треба помістити компоненти: GreetingBlock, StatusBlock, BabyTodayCard,
-        MomTipCard, TasksReminderCard, FeelingCheckCard,
-      </div>
+    <div className={css.container}>
+      <section>GreetingBlock</section>
+      <section>StatusBlock</section>
+      <section>BabyTodayCard</section>
+      <section>MomTipCard</section>
+      <section>TasksReminderCard</section>
+      <section>FeelingCheckCard</section>
     </div>
   )
 }
