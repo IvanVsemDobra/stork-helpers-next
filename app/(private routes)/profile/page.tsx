2eeb26b1fffe88a600065392143a3e6c3915d30a
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2026-01-14 17:50:09 +0100

    fix: update profile components

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import ProfileAvatar from '@/components/ProfileAvatar/ProfileAvatar'
-import ProfileEditForm from '@/components/ProfileEditForm/ProfileEditForm'
+import { ProfileAvatar } from '@/components/ProfileAvatar/ProfileAvatar'
+import { ProfileEditForm } from '@/components/ProfileEditForm/ProfileEditForm'
 import { useAuthStore } from '@/store/auth.store'
 import { useProtectedRoute } from '@/hooks/useProtectedRoute'
 
