e600bf2fdb3b5f89c55fbc1763583d3e7223f59f
Olena Kushch <ellinate@gmail.com>
2026-01-15 19:18:14 +0200

    fixes all files tasks

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,14 @@
 import axios from 'axios'
 
+const isServer = typeof window === 'undefined'
+const BASE_URL = isServer
+    ? process.env.NEXT_PUBLIC_API_URL || 'https://stork-helpers-api.onrender.com/api'
+    : '/api'
+
 export const api = axios.create({
-    baseURL: process.env.API_URL, // 🔥 ОБОВʼЯЗКОВО
+    baseURL: BASE_URL,
     withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 })
