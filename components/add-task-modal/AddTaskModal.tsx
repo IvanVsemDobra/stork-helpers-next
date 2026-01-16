ed3a4c41553445f656850d9f696aa3d6aca54521
Olena Kushch <ellinate@gmail.com>
2026-01-16 09:00:19 +0200

    fixes path/proxy, all task files

--- components/add-task-modal/AddTaskModal.tsx
+++ components/add-task-modal/AddTaskModal.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useEffect } from 'react'
 import AddTaskForm from './AddTaskForm'
 import styles from './AddTaskModal.module.css'
@@ -9,12 +11,12 @@ interface AddTaskModalProps {
   onTaskSaved: () => void
   taskToEdit?: {
     id: string
-    title: string
+    name: string
     date: string
   } | null
 }
 
-const AddTaskModal = ({ isOpen, onClose, taskToEdit }: AddTaskModalProps) => {
+const AddTaskModal = ({ isOpen, onClose, taskToEdit, onTaskSaved }: AddTaskModalProps) => {
   useEffect(() => {
     const onEsc = (e: KeyboardEvent) => {
       if (e.key === 'Escape') onClose()
@@ -42,7 +44,7 @@ const AddTaskModal = ({ isOpen, onClose, taskToEdit }: AddTaskModalProps) => {
 
         <h2 className={styles.title}>{taskToEdit ? 'Редагувати завдання' : 'Нове завдання'}</h2>
 
-        <AddTaskForm onClose={onClose} taskToEdit={taskToEdit} />
+        <AddTaskForm /* onClose={onClose} */ taskToEdit={taskToEdit} onTaskSaved={onTaskSaved}/>
       </div>
     </div>
   )
