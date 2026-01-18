e14b28bf1b1814dcb5447cc443530a16e6e4c2ef
Anton Zorkin <zanton1978@gmail.com>
2026-01-18 11:54:35 +0100

    Resolve MomTipCard stash conflict

--- components/MomTipCard/mom-tip-card.tsx
+++ components/MomTipCard/mom-tip-card.tsx
@@ -7,14 +7,10 @@ interface momDailyTipsProps {
 export const MomTipCard = ({ tipIndex, momDailyTips }: momDailyTipsProps) => {
   return (
     <section className={css.card}>
-      
       <h2 className={css.title}>Порада для мами</h2>
       <p className={css.text}>
         {momDailyTips[tipIndex] ? momDailyTips[tipIndex] : 'Порада відсутня'}
       </p>
- 
     </section>
-    
   )
 }
-
