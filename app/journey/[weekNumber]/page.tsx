685cc4f0a117aad6f6fbcf8512c73d51c49e2827
Nataliia Tkachenko <tkachenko_nataliya83@ukr.net>
2026-01-18 18:41:56 +0200

    feature/JourneyPage-logic

--- app/journey/[weekNumber]/page.tsx
+++ app/journey/[weekNumber]/page.tsx
@@ -1,3 +1,14 @@
-export default function weekNumber() {
-  return <div>JourneyDetails component</div>
+import { GreetingBlock } from '@/components/GreetingBlock/GreetingBlock'
+
+import JourneyDetails from '@/components/JourneyDetails/JourneyDetails'
+
+export default function JourneyWeekPage({ params }: { params: { weekNumber: string } }) {
+  const weekNumber = Number(params.weekNumber)
+
+  return (
+    <>
+      <GreetingBlock />
+      <JourneyDetails weekNumber={weekNumber} />
+    </>
+  )
 }
--- app/journey/page.tsx
+++ app/journey/page.tsx
@@ -1,16 +1,6 @@
-'use client'
+import { redirect } from 'next/navigation'
 
-import { GreetingBlock } from '@/components/GreetingBlock/GreetingBlock'
-import JourneyDetails from '@/components/JourneyDetails/JourneyDetails'
-import WeekSelector from '@/components/WeekSelector/WeekSelector'
-
-export default function JourneyPage() {
-  return (
-    <div>
-      <GreetingBlock />
-      <WeekSelector currentWeek={4} selectedWeek={5} onWeekSelect={() => {}} />
-
-      <JourneyDetails />
-    </div>
-  )
+export default function JourneyIndexPage() {
+  const currentWeek = 12 // тимчасово
+  redirect(`/journey/${currentWeek}`)
 }
