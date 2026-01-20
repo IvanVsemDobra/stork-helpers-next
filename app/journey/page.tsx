f863d4136b09f1ddac63f409b06b7b08cb72e0bb
Jeka <gekanefed22.04.2010@gmail.com>
2026-01-20 14:03:53 +0200

    upate

--- app/journey/page.tsx
+++ app/journey/page.tsx
@@ -1,8 +1,26 @@
-import { getMyDayWeekInfo } from '@/services/server/weeks.server'
-import { redirect } from 'next/navigation'
+'use client'
 
-export default async function JourneyPage() {
-  const weekData = await getMyDayWeekInfo()
-  console.log(weekData?.weekNumber)
-  redirect(`/journey/${weekData?.weekNumber}`)
+import React, { useState } from 'react'
+import { GreetingBlock } from '@/components/GreetingBlock/GreetingBlock'
+import JourneyDetails from '@/components/JourneyDetails/JourneyDetails'
+import WeekSelector from '@/components/WeekSelector/WeekSelector'
+import css from './JourneyPage.module.css'
+
+export default function JourneyPage() {
+
+  const [currentWeek] = useState(25)
+  const [selectedWeek, setSelectedWeek] = useState(25)
+
+
+  return (
+    <div className={css.container}>
+      <GreetingBlock />
+      <WeekSelector
+        currentWeek={currentWeek}
+        selectedWeek={selectedWeek}
+        onWeekSelect={setSelectedWeek}
+      />
+      <JourneyDetails weekNumber={selectedWeek} />
+    </div>
+  )
 }
